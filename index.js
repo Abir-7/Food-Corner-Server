5328cfe7f9e291f3c911208223962f7b444df3c1
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2023-10-18 20:58:54 +0600

    add menu & get menu api and other

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  // console.log(authorization)
+  console.log(authorization)
   if (!authorization) {
     console.log('1')
     return res.status(401).send({ error: true, message: 'unauthorized access' });
@@ -34,7 +34,7 @@ const verifyJWT = (req, res, next) => {
 }
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o5aamuw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,41 +54,60 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("Food_Corner").collection("users");
+    const menuCollection = client.db("Food_Corner").collection("allMenu");
+
+
+
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_Token, { expiresIn: '6h' })
+      const token = jwt.sign(user, process.env.ACCESS_Token, { expiresIn: '10h' })
       res.send({ token })
     })
 
 
     //use verifyJWT before using verifyAdmin
     const verifyAdmin = async (req, res, next) => {
+      console.log('verify admin')
       const email = req.decoded.email;
+
       const query = { email: email }
       const user = await usersCollection.findOne(query);
-      console.log(user)
-      if (user?.role !== 'admin') {
-        return res.status(403).send({ error: true, message: 'forbidden message' });
+
+      if (user) {
+        if (user?.role !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
       }
       next();
     }
 
 
     app.get('/', (req, res) => {
+      console.log('hi')
       res.send('Hello World!')
     })
 
     ////////////////////////---USER API---/////////////////////////
     app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      console.log('get user')
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
 
+    app.get('/singleUsers', verifyJWT, async (req, res) => {
+      console.log('get single  user')
+      const email = req.decoded.email;
+
+      const query = { email: email }
+      const result = await usersCollection.findOne(query)
+
+      res.send(result)
+    })
+
     app.post('/users', async (req, res) => {
-      console.log('hi')
+      console.log('add  user')
       const data = req.body
-      console.log(data, '111')
 
       const query = { email: data.email }
       const existingUser = await usersCollection.findOne(query)
@@ -99,27 +118,87 @@ async function run() {
       const result = await usersCollection.insertOne(data)
       res.send(result)
     })
+
+    app.patch('/userUpdate', verifyJWT, async (req, res) => {
+      console.log('update  user')
+      console.log('hi')
+      const email = req.decoded.email;
+
+      const data = req.body
+      console.log(data, '111')
+
+      const filter = { email: email };
+
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          mobile: data.mobile,
+          address: data.address,
+          image: data.image,
+        },
+      }
+      const options = { upsert: true };
+
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+
+    })
+
     ////////////////////////---USER API END---/////////////////////////
 
+
+
     ////////////////////////---Admin API ---/////////////////////////
-    app.get('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
-      const email = req.params.email
+    app.get('/user/admin', verifyJWT, verifyAdmin, async (req, res) => {
+      console.log('get admin or user')
+      const email = req.decoded.email;
       const result = await usersCollection.findOne({ email: email })
-      console.log(result.role)
-      if (result.role === 'admin') {
-        res.send( true )
+      // console.log(result?.role, '145')
+      if (result?.role === 'admin') {
+        console.log('get admin or user2')
+        res.status(200).send(true)
+        //res.send(true)
       }
       else {
-        res.send(false )
+        console.log('get admin or user 3')
+        res.status(403).send(false)
       }
-
-      //res.send(result)
     })
+
 
     ////////////////////////---Admin API END ---/////////////////////////
 
 
+    /////////////////////////---Menu Item Api---///////////////////////
 
+    //Add Menu//
+    app.post('/addMenu', verifyJWT, verifyAdmin, async (req, res) => {
+      console.log('add menu')
+      const data = req.body
+      //console.log(data,';;;;')
+      const result = await menuCollection.insertOne(data)
+      res.send(result)
+    })
+
+    app.get('/getMenu', async (req, res) => {
+      console.log('get menu')
+      const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/getMenu/:id', verifyJWT,async (req, res) => {
+      console.log('get single menu')
+
+      const id = req.params.id
+      console.log(id,'id')
+      const query={_id:new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      console.log(result)
+      res.send(result)
+    })
+
+
+    //////////////////////////---Menu Api End---////////////////
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
     })
@@ -132,3 +211,4 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
