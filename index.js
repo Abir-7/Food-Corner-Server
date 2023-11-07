f154f9a4c8fa01ed48e55fcc1a879e55a7b5aa52
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2023-11-07 21:13:59 +0600

    fix fav api bug

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  ////////console.log(authorization)
+  ////////////console.log(authorization)
   if (!authorization) {
-    ////////console.log('1')
+    ////////////console.log('1')
     return res.status(401).send({ error: true, message: 'unauthorized access' });
   }
   // bearer token
@@ -27,7 +27,7 @@ const verifyJWT = (req, res, next) => {
 
   jwt.verify(token, process.env.ACCESS_Token, (err, decoded) => {
     if (err) {
-      ////////console.log('2')
+      ////////////console.log('2')
       return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
     req.decoded = decoded;
@@ -82,15 +82,15 @@ async function run() {
 
     //use verifyJWT before using verifyAdmin
     const verifyAdmin = async (req, res, next) => {
-      ////////console.log('verify admin')
+      ////////////console.log('verify admin')
       const email = req.decoded.email;
-      ////console.log(email, 'veryfy admin')
+      ////////console.log(email, 'veryfy admin')
       const query = { email: email }
       const user = await usersCollection.findOne(query);
 
       if (user) {
         if (user?.role !== 'admin') {
-          ////console.log('false section')
+          ////////console.log('false section')
           return res.status(403).send({ error: true, message: 'forbidden Access', isAdmin: false });
         }
       }
@@ -99,7 +99,7 @@ async function run() {
 
 
     app.get('/', (req, res) => {
-      ////////console.log('hi')
+      ////////////console.log('hi')
       res.send('Hello World!')
     })
 
@@ -112,7 +112,7 @@ async function run() {
 
       const { price } = req.body;
       const amount = price * 100
-      //////console.log(email, 'line 96', amount)
+      //////////console.log(email, 'line 96', amount)
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
@@ -147,7 +147,7 @@ async function run() {
           const query1 = { status: 'Delivered' }
           const result = await paymentCollection.find(query).toArray()
           const result1 = await paymentCollection.find(query1).toArray()
-          console.log(result, result1)
+          ////console.log(result, result1)
           return res.send({ result, result1 })
         }
         else {
@@ -169,7 +169,7 @@ async function run() {
 
           const result = await paymentCollection.find(query).toArray()
           const result1 = await paymentCollection.find(query1).toArray()
-          console.log(result1)
+          ////console.log(result1)
           return res.send({ result, result1 })
         }
       }
@@ -184,11 +184,11 @@ async function run() {
     app.patch('/modifyOrderStatus', verifyJWT, verifyAdmin, async (req, res) => {
       const status = req.body.status
       const paymentID = req.body.paymentID
-      ////console.log(status, paymentID)
+      ////////console.log(status, paymentID)
 
       const providedEmail = req.body.userEmail
       const decodedEmail = req.decoded.email
-      ////console.log(providedEmail,decodedEmail)
+      ////////console.log(providedEmail,decodedEmail)
       if (decodedEmail == providedEmail) {
 
         const filter = { paymentID: paymentID };
@@ -203,7 +203,7 @@ async function run() {
         const options = { upsert: true };
 
         const result = await paymentCollection.updateOne(filter, updateDoc, options)
-        ////console.log(result)
+        ////////console.log(result)
         return res.send(result)
       }
 
@@ -257,7 +257,7 @@ async function run() {
 
     ////////////////////////---USER API---/////////////////////////
     app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
-      ////////console.log('get user')
+      ////////////console.log('get user')
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
@@ -265,7 +265,7 @@ async function run() {
 
 
     app.get('/singleUsers', verifyJWT, async (req, res) => {
-      ////////console.log('get single  user')
+      ////////////console.log('get single  user')
       const email = req.decoded.email;
 
       const query = { email: email }
@@ -276,20 +276,20 @@ async function run() {
 
 
     app.get('/singleUsers/:email', verifyJWT, async (req, res) => {
-      //console.log('get single  user')
+      //////console.log('get single  user')
       const providedEmail = req.params.email
       const email = req.decoded.email;
 
       const query = { email: providedEmail }
       const result = await usersCollection.findOne(query)
-      //console.log(result,providedEmail)
+      //////console.log(result,providedEmail)
       res.send(result)
     })
 
 
 
     app.post('/users', async (req, res) => {
-      ////////console.log('add  user')
+      ////////////console.log('add  user')
       const data = req.body
 
       const query = { email: data.email }
@@ -303,12 +303,12 @@ async function run() {
     })
 
     app.patch('/userUpdate', verifyJWT, async (req, res) => {
-      ////////console.log('update  user')
-      ////////console.log('hi')
+      ////////////console.log('update  user')
+      ////////////console.log('hi')
       const email = req.decoded.email;
 
       const data = req.body
-      ////////console.log(data, '111')
+      ////////////console.log(data, '111')
 
       const filter = { email: email };
 
@@ -333,20 +333,20 @@ async function run() {
 
     ////////////////////////---Admin API ---/////////////////////////
     app.get('/user/admin', verifyJWT, verifyAdmin, async (req, res) => {
-      ////console.log('get admin or user')
+      ////////console.log('get admin or user')
 
       const email = req.decoded.email;
-      ////console.log(email, 'line 193')
+      ////////console.log(email, 'line 193')
       if (email) {
         const result = await usersCollection.findOne({ email: email })
-        ////console.log(result?.role, '145')
+        ////////console.log(result?.role, '145')
         if (result?.role === 'admin') {
-          ////console.log('true section')
+          ////////console.log('true section')
           res.status(200).send(true)
 
         }
         else {
-          ////////console.log('get admin or user 3')
+          ////////////console.log('get admin or user 3')
           res.status(403).send(false)
         }
       }
@@ -360,23 +360,23 @@ async function run() {
 
     //Add Menu//
     app.post('/addMenu', verifyJWT, verifyAdmin, async (req, res) => {
-      ////////console.log('add menu')
+      ////////////console.log('add menu')
       const data = req.body
-      //////////console.log(data,';;;;')
+      //////////////console.log(data,';;;;')
       const result = await menuCollection.insertOne(data)
       res.send(result)
     })
 
     app.get('/getMenu', async (req, res) => {
 
-      const category=req.query.name
-      const cuisine=req.query.cuisine
+      const category = req.query.name
+      const cuisine = req.query.cuisine
 
       const query = {
         $and: [
-         cuisine!=='all'? { cuisine: cuisine }:{},
-         category !== 'all' ? { category: category } : {},
-        
+          cuisine !== 'all' ? { cuisine: cuisine } : {},
+          category !== 'all' ? { category: category } : {},
+
         ]
       }
 
@@ -390,10 +390,10 @@ async function run() {
         const average = count > 0 ? sum / count : 0;
         item.averageRating = average;
       });
-    
+
       await Promise.all(ratingPromises);
-    
-      console.log(result);
+
+      ////console.log(result);
       res.send(result)
     })
 
@@ -407,12 +407,12 @@ async function run() {
         const count = ratings.length;
         const average = count > 0 ? sum / count : 0;
         item.averageRating = average;
-        item.totalCustomer=count
+        item.totalCustomer = count
       });
-    
+
       await Promise.all(ratingPromises);
-    
-      console.log(result);
+
+      ////console.log(result);
 
       result.sort((a, b) => {
         if (b.averageRating !== a.averageRating) {
@@ -421,13 +421,14 @@ async function run() {
         return b.totalCustomer - a.totalCustomer;
       })
 
-      
+
       const limitedResult = result.slice(0, 4)
       res.send(limitedResult)
     })
 
 
     app.get('/thaiCuisine', async (req, res) => {
+      const email = req.query.email
       const query = {
         $and: [
           { cuisine: 'Thai' },
@@ -436,6 +437,8 @@ async function run() {
         ]
       }
       const result = await menuCollection.find(query).toArray()
+
+
       //res.send(result)
       const ratingPromises = result.map(async (item) => {
         const { _id } = item;
@@ -444,9 +447,9 @@ async function run() {
         const count = ratings.length;
         const average = count > 0 ? sum / count : 0;
         item.averageRating = average;
-        item.totalCustomer=count
+        item.totalCustomer = count
       });
-    
+
       await Promise.all(ratingPromises);
 
       const randomizedResult = shuffleArray(result)
@@ -456,15 +459,17 @@ async function run() {
       const arrayOfIds = randomFoods.map(food => new ObjectId(food._id).toString());
 
       // Use aggregation to find documents with matching IDs in FavMenu collection
+      console.log(email)
       const matchingDocs = await favouriteMenuCollection.aggregate([
         {
           $match: {
-            menuID: { $in: arrayOfIds }
+            menuID: { $in: arrayOfIds },
+            userEmail: email
           }
         }
       ]).toArray();
 
-      //console.log(matchingDocs,)
+      //////console.log(matchingDocs,)
       // Update randomFoods with match field
       randomFoods.forEach(food => {
         food.match = matchingDocs.some(doc => doc.menuID === new ObjectId(food._id).toString());
@@ -476,64 +481,64 @@ async function run() {
 
 
     app.get('/getMenu/:id', verifyJWT, async (req, res) => {
-      //////console.log('get single menu')
+      //////////console.log('get single menu')
 
       const id = req.params.id
       const email = req.query.email
-      //console.log(id, 'id',email)
+      //////console.log(id, 'id',email)
       const query = { _id: new ObjectId(id) }
       const result = await menuCollection.findOne(query)
 
-      const {_id}=result
+      const { _id } = result
 
-      const ratings=await reviewCollection.find({menuID:new ObjectId(_id).toString()}).toArray()
-      const sum=ratings.reduce((total,rating)=>total+rating.rating,0)
-      const count=ratings.length
-      const average=count>0?sum/count:0 
+      const ratings = await reviewCollection.find({ menuID: new ObjectId(_id).toString() }).toArray()
+      const sum = ratings.reduce((total, rating) => total + rating.rating, 0)
+      const count = ratings.length
+      const average = count > 0 ? sum / count : 0
       result.averageRating = average;
-      result.totalCustomer=count
-      //console.log(result)
+      result.totalCustomer = count
+      //////console.log(result)
       res.send(result)
     })
 
 
     app.get('/getSimilarMenu/:category', verifyJWT, async (req, res) => {
-      //////console.log('get single menu')
+      //////////console.log('get single menu')
 
-      const category=req.params.category
-      const id=req.query.id
+      const category = req.params.category
+      const id = req.query.id
       const query = {
         $and: [
-          {category:category},
-          {_id: { $ne: new ObjectId(id) } }
+          { category: category },
+          { _id: { $ne: new ObjectId(id) } }
         ]
       }
-      
-      const result=await menuCollection.find(query).toArray()
 
-      const ratingPromise=result.map(async(item)=>{
-        const {_id}=item
+      const result = await menuCollection.find(query).toArray()
 
-        const ratings=await reviewCollection.find({menuID:new ObjectId(_id).toString()}).toArray()
-        
-        const sum=ratings?ratings.reduce((total,rating)=>total+rating.rating,0):0
-        const count=ratings.length
-        const average=count>0?sum/count:0 
+      const ratingPromise = result.map(async (item) => {
+        const { _id } = item
+
+        const ratings = await reviewCollection.find({ menuID: new ObjectId(_id).toString() }).toArray()
+
+        const sum = ratings ? ratings.reduce((total, rating) => total + rating.rating, 0) : 0
+        const count = ratings.length
+        const average = count > 0 ? sum / count : 0
         item.averageRating = average;
-        item.totalCustomer=count
+        item.totalCustomer = count
       })
 
       await Promise.all(ratingPromise)
 
-      console.log(result)
+      ////console.log(result)
 
       res.send(result)
     })
 
     app.post('/addFavMenu', verifyJWT, async (req, res) => {
-      //////console.log('add to favourite')
+      //////////console.log('add to favourite')
       const menuId = req.body
-      //////console.log(menuId)
+      //////////console.log(menuId)
 
       const query = {
         $and: [
@@ -555,10 +560,10 @@ async function run() {
     })
 
     app.get('/favMenu/:id', verifyJWT, async (req, res) => {
-      //////console.log('is favourite or not')
+      //////////console.log('is favourite or not')
       const id = req.params.id
       const email = req.decoded.email;
-      //////console.log(id, email, '---')
+      //////////console.log(id, email, '---')
       const query = {
         $and: [
           { menuID: id },
@@ -568,33 +573,33 @@ async function run() {
       const existingItem = await favouriteMenuCollection.findOne(query)
 
       if (existingItem) {
-        //////console.log('true')
+        //////////console.log('true')
         return res.send({ result: true })
       }
       else {
-        //////console.log('false')
+        //////////console.log('false')
         return res.send({ result: false })
 
       }
     })
 
     app.get('/favMenuData/:email', verifyJWT, async (req, res) => {
-      //////console.log('get user all fav data')
+      //////////console.log('get user all fav data')
       const email = req.decoded.email || req.params.email
-      //////console.log(email)
+      //////////console.log(email)
       const query = { userEmail: email }
       const favMenuByUser = await favouriteMenuCollection.find(query).toArray()
       const favMenuId = favMenuByUser.map(menuId => new ObjectId(menuId.menuID))
       const result = await menuCollection.find({ _id: { $in: favMenuId } }).toArray()
-      //////console.log(result)
+      //////////console.log(result)
       res.send(result)
     })
 
     app.delete('/deleteFavMenu', verifyJWT, async (req, res) => {
-      //////console.log('delete user single fav data')
+      //////////console.log('delete user single fav data')
       const email = req.decoded.email || req.query.email
       const menuId = req.query.menuId
-      //////console.log(email, menuId)
+      //////////console.log(email, menuId)
 
       const query = {
         $and: [
@@ -604,24 +609,24 @@ async function run() {
       }
 
       const result = await favouriteMenuCollection.deleteOne(query)
-      //console.log(result, 'delete')
+      //////console.log(result, 'delete')
       res.send(result)
     })
 
 
-    app.patch('/modifyAvailableStatus/:id',async(req,res)=>{
-      const id =req.params.id
+    app.patch('/modifyAvailableStatus/:id', async (req, res) => {
+      const id = req.params.id
 
       const filter = { _id: new ObjectId(id) };
-      const status=req.body.status
+      const status = req.body.status
       const updateDoc = {
         $set: {
-        isAvailable:status
+          isAvailable: status
         },
       }
       const options = { upsert: true };
       const result = await menuCollection.updateOne(filter, updateDoc, options)
-res.send(result)
+      res.send(result)
     })
 
 
@@ -663,17 +668,17 @@ res.send(result)
 
     app.get('/getReviews/:id', async (req, res) => {
       const id = req.params.id
-      console.log(id)
+      ////console.log(id)
       const query = { menuID: id }
       const result = await reviewCollection.find(query).toArray()
-      //console.log(result)
+      //////console.log(result)
       res.send(result)
     })
 
 
     app.post('/addFeedback', verifyJWT, async (req, res) => {
       const data = req.body
-      console.log(data)
+      ////console.log(data)
       // const query = {
       //   $and: [
       //     {
@@ -696,7 +701,7 @@ res.send(result)
       // else {
       //   const result = await reviewCollection.insertOne(data)
       //   res.send(result)
-     
+
       // }
 
       const result = await feedbackCollection.insertOne(data)
@@ -706,7 +711,7 @@ res.send(result)
 
     app.get('/getFeedback', async (req, res) => {
       const result = await feedbackCollection.find().toArray()
-      //console.log(result)
+      //////console.log(result)
       res.send(result)
     })
 
@@ -716,11 +721,11 @@ res.send(result)
 
 
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
+      ////console.log(`Example app listening on port ${port}`)
     })
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    ////console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
